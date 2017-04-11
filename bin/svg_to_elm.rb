a70b6d97ac3325617e467852623e026399e82d50
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2017-04-11 10:43:57 -0700

    Handle more svg_to_elm cases.
    Why:
    
    * There are many edge cases that require a fully qualified function name.
    * There are a few svg elements / attributes that need `_` appended.
    * We need some whitespace in order for Elm format to work.
    * Namespaced elements need to be converted correctly (e.g. xlink:href).
    
    This change addresses the need by:
    
    * Fully qualify all function calls.
    * Append `_` to SPECIAL_NAMES and SPECIAL_ATTRS.
    * Add some whitepsace to the output.
    * Add namespace prefixes to the function names if there is one.

--- bin/svg_to_elm.rb
+++ bin/svg_to_elm.rb
@@ -4,37 +4,49 @@ require 'nokogiri'
 
 raise 'You must pass the svg to STDIN' if STDIN.tty?
 
-QUALIFY_NAME = ARGV.shift
-QUALIFY = ARGV.to_a
-
 svg = Nokogiri::XML(STDIN)
+
+SPECIAL_NAMES = %w(text)
+SPECIAL_ATTRS = %w(in type)
+
+def namespace_name(ns, name)
+  return name unless ns&.prefix
+  name[0] = name[0].upcase
+  ns.prefix + name
+end
 
 def elem_name(elem)
   name = elem.name
-  QUALIFY.include?(name) ? "#{QUALIFY_NAME}.#{name}" : name
+  name = "#{name}_" if SPECIAL_NAMES.include?(name)
+  "Svg.#{namespace_name(elem.namespace, name)}"
 end
 
 def attr_name(attr)
+  attr = "#{attr}_" if SPECIAL_ATTRS.include?(attr)
   attr.gsub(/-(.)/) { Regexp.last_match[1].upcase }
 end
 
-def get_attrs(elem)
+def attrs(elem)
   elem.attributes.map do |_, attr|
-    %(#{attr_name attr.name} "#{attr.value}")
+    name = namespace_name(attr.namespace, attr_name(attr.name))
+    %(Svg.Attributes.#{name} "#{attr.value}")
   end
 end
 
-def get_children(elem)
+def children(elem)
   elem.children.map { |child| elem2elm child }.compact
 end
 
 def elem2elm(elem)
-  attrs = get_attrs(elem).join(',')
-  children = get_children(elem).join(',')
+  return %(Svg.text "#{elem.to_s}") if elem.class == Nokogiri::XML::Text && !elem.to_s.strip.empty?
+
+  line_end = "\n  "
+  attrs = attrs(elem).join(",#{line_end}")
+  children = children(elem).join(",#{line_end}")
 
   return if attrs.empty? && children.empty?
 
-  "#{elem_name elem} [#{attrs}] [#{children}]"
+  "  #{elem_name elem}#{line_end}[#{line_end}#{attrs}#{line_end}]#{line_end}[#{line_end}#{children}#{line_end}]"
 end
 
 puts elem2elm(svg.child)
